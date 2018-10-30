3820c31cfceb052d685f3072f6f4421cfaba56d1
Marcell Guilherme Costa da Silva <marcell@evolux.net.br>
2018-10-30 20:16:33 -0300

    replace whole site by react-react-app boilerplate

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import ScrollNavigation from 'scroll-navigation-menu';
-
-addEventListener('load', () => {
-  const offset = -document.querySelector('.header').getBoundingClientRect().height;
-  new ScrollNavigation({ offset }).start();
-  document.body.style.paddingTop = `${(offset * -1) + 2}px`;
-});
