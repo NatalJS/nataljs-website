70426582c23bcab99149135a2d851f341845331c
David Costa <davidcostadev@gmail.com>
2019-08-23 12:38:36 -0300

    Using events dynamically

--- components/signUp.js
+++ components/signUp.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const SignUp = () => (
-  <React.Fragment>
-    <a
-      href="https://docs.google.com/forms/d/e/1FAIpQLSdWT3MnHCGjhBp7dn5KYTfd61eysoO93TB8Alz12u2vmXiJ-Q/viewform"
-      className="btn btn--white w-100 text-center"
-      target="_blank"
-    >
-      inscreva-se agora
-    </a>
-  </React.Fragment>
+const SignUp = ({ subscriptionLink }) => (
+  <a
+    href={subscriptionLink}
+    className="btn btn--white w-100 text-center"
+    title="Inscreva-se agora"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Inscreva-se agora
+  </a>
 );
 
 export default SignUp;
--- tests/components/Events.test.js
+++ tests/components/Events.test.js
@@ -1,12 +1,13 @@
 
 import renderer from 'react-test-renderer';
 import Events from 'components/Events';
+import data from '../../content/data.json';
 
 describe('Component Events', () => {
   let Component;
 
   beforeAll(() => {
-    Component = setup(Events);
+    Component = setup(Events, { events: data.events });
   });
 
   it('should render component correctly', () => {
