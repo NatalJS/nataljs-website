555d81b10156fd07ccbd759fdaba1dd7629e9f9b
David Costa <davidcostadev@gmail.com>
2019-08-23 13:27:46 -0300

    Update the next event informations and add call4papers (#14)

--- components/CallForPapers.js
+++ components/CallForPapers.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const CallForPapers = () => (
+const CallForPapers = ({ link }) => (
   <section id="call-for-papers" className="call-for-papers">
     <h1 className="section-title">Call For Papers</h1>
     <p>Tem uma palestra legal? Tem algo para falar?</p>
-    <a href="http://speakerfight.com/events/ii-evento-nataljs" className="btn btn--black" targe="_blank">
+    <a href={link} className="btn btn--black" targe="_blank">
       Submeta sua palestra
     </a>
   </section>
